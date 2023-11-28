5a05ac55ed1e78063079b0f671256e63d35f177a
ktkafy <redteamsec2022@gmail.com>
2023-11-28 23:43:57 +0330

    arguments

--- 01-mac_changer.py
+++ 01-mac_changer.py
@@ -14,4 +14,22 @@ subprocess.call("ifconfig {} up".format(interface), shell=True)
 
 #get all necessary data from user in arguments
 parser = optparse.OptionParser()
-parser.add_option("-i", "--interface", dest="interface") #here we say user either can use -i or --interface
+
+#interface
+parser.add_option("-i", "--interface", dest="interface", help="interface to change its MAC address") #here we say user either can use -i or --interface
+
+#mac address
+parser.add_option("-m", "--mac", dest="new_mac", help="new MAC address") #here we say user either can use -i or --interface
+
+#to capture these variables, we're gonna take wathever parser gets
+(options, arguments) = parser.parse_args()
+
+#take those options in action
+interface = options.interface
+new_mac = options.new_mac
+
+print("[+] changing mac address for {} with {}".format(interface, new_mac))
+
+subprocess.call("ifconfig {} down".format(interface),shell=True)
+subprocess.call("ifconfig {} hw ether {}".format(interface, new_mac),shell=True)
+subprocess.call("ifconfig {} up".format(interface), shell=True)
