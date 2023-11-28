79c9e06f9029b36d850f9e9fdca87f5f3bd13e92
ktkafy <redteamsec2022@gmail.com>
2023-11-28 23:20:33 +0330

    arguments

--- 01-mac_changer.py
+++ 01-mac_changer.py
@@ -1,6 +1,8 @@
 import subprocess
-interface = "eth0"
-new_mac = "00:11:22:33:44:55"
+import optparse #module to let us use arguments and parse them and use them
+#in a fashion mode to get input directly from user
+"""interface = input("[+]enter the desired interface: ")
+new_mac = input("[+]enter your desired MAC address: ")
 
 print("[+] changing mac address for {} with {}".format(interface, new_mac))
 
@@ -8,3 +10,8 @@ print("[+] changing mac address for {} with {}".format(interface, new_mac))
 subprocess.call("ifconfig {} down".format(interface),shell=True)
 subprocess.call("ifconfig {} hw ether {}".format(interface, new_mac),shell=True)
 subprocess.call("ifconfig {} up".format(interface), shell=True)
+"""
+
+#get all necessary data from user in arguments
+parser = optparse.OptionParser()
+parser.add_option("-i", "--interface", dest="interface") #here we say user either can use -i or --interface
