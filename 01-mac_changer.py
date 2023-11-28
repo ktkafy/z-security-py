58a9ef77b373f474ba99c6dceddb0ad6cd1caeae
ktkafy <redteamsec2022@gmail.com>
2023-11-29 00:19:16 +0330

    argument

--- 01-mac_changer.py
+++ 01-mac_changer.py
@@ -12,24 +12,38 @@ subprocess.call("ifconfig {} hw ether {}".format(interface, new_mac),shell=True)
 subprocess.call("ifconfig {} up".format(interface), shell=True)
 """
 
-#get all necessary data from user in arguments
-parser = optparse.OptionParser()
+def change_mac(interface, new_mac):
+    print("[+] changing mac address for {} with {}".format(interface, new_mac))
 
-#interface
-parser.add_option("-i", "--interface", dest="interface", help="interface to change its MAC address") #here we say user either can use -i or --interface
+    subprocess.call("ifconfig {} down".format(interface),shell=True)
+    subprocess.call("ifconfig {} hw ether {}".format(interface, new_mac),shell=True)
+    subprocess.call("ifconfig {} up".format(interface), shell=True)
 
-#mac address
-parser.add_option("-m", "--mac", dest="new_mac", help="new MAC address") #here we say user either can use -i or --interface
+def get_arguments():
+    #get all necessary data from user in arguments
+    parser = optparse.OptionParser()
 
-#to capture these variables, we're gonna take wathever parser gets
+    #interface
+    parser.add_option("-i", "--interface", dest="interface", help="interface to change its MAC address") #here we say user either can use -i or --interface
+
+    #mac address
+    parser.add_option("-m", "--mac", dest="new_mac", help="new MAC address") #here we say user either can use -i or --interface
+
+    #we use return so that the parsed arguments are available through the program
+    return parser.parse_args()
+
+"""
+#to capture these variables passed for the program, we're gonna take wathever parser gets
+#this is done when we use tradiotional way and not using function
 (options, arguments) = parser.parse_args()
+as we are using return inside the function, we now may use the code like below to get result of parser.parse_args()
+"""
+(options,arguments) = get_arguments()
 
-#take those options in action
+"""
+take those options in action; when using function we don't need to announce them
 interface = options.interface
 new_mac = options.new_mac
+"""
 
-print("[+] changing mac address for {} with {}".format(interface, new_mac))
-
-subprocess.call("ifconfig {} down".format(interface),shell=True)
-subprocess.call("ifconfig {} hw ether {}".format(interface, new_mac),shell=True)
-subprocess.call("ifconfig {} up".format(interface), shell=True)
+change_mac(options.interface,options.new_mac)
