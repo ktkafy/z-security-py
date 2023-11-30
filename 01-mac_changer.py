950b607e8ecb599aaff11c3dc905bb5d82587a45
ktkafy <redteamsec2022@gmail.com>
2023-11-30 10:39:39 +0330

    arguments

--- 01-mac_changer.py
+++ 01-mac_changer.py
@@ -30,7 +30,15 @@ def get_arguments():
     parser.add_option("-m", "--mac", dest="new_mac", help="new MAC address") #here we say user either can use -i or --interface
 
     #we use return so that the parsed arguments are available through the program
-    return parser.parse_args()
+    #return parser.parse_args() #commented due to filter out those scenarios in which user has not entered value for the arguments
+    
+    #see if user has entered a value for arguments
+    (options,arguments) = parser.parse_args()
+    if not options.interface:
+        parser.error("[+] please specify your desired interface. use --help to see argument's name")
+    elif not options.new_mac:
+        parser.error("[+] please specify your desired MAC address. use --help to see argument's name")
+    return options
 
 """
 #to capture these variables passed for the program, we're gonna take wathever parser gets
@@ -38,7 +46,11 @@ def get_arguments():
 (options, arguments) = parser.parse_args()
 as we are using return inside the function, we now may use the code like below to get result of parser.parse_args()
 """
+#when we were using without filtering for arguments:
 (options,arguments) = get_arguments()
+
+#use only options when we have filtered out those which have all neccessary arguments
+options = get_arguments()
 
 """
 take those options in action; when using function we don't need to announce them
