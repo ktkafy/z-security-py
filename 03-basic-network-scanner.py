cd1b62411aa4eb293f66e78d662c1cf127b2b05c
ktkafy <redteamsec2022@gmail.com>
2023-12-10 23:40:01 +0330

    scan

--- 03-basic-network-scanner.py
+++ 03-basic-network-scanner.py
@@ -6,7 +6,7 @@ def scan(ip):
     #scapy.arping(ip)
     
     #making arp packet using scapy
-    arpRequest = scapy.ARP()
+    #arpRequest = scapy.ARP()
     arpRequest = scapy.ARP(pdst=ip) #added to get data of pdst directly and avoiding adding another instance for the variable
     
     #setting IP value for our arp packet to replace default value of 0.0.0.0
@@ -16,4 +16,16 @@ def scan(ip):
     #scapy.ls(scapy.ARP())
     print(arpRequest.summary())
 
-scan("10.0.2.1/24")
+def scan_2_send_2_boradcast_mac(ip):
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request
+    #print(arp_request_broadcast.summary()) #show summary of our packet
+    #arp_request_broadcast.show() #show details about out packet
+    
+    #to get available fields for scapy.Ether
+    #scapy.ls(scapy.Ether())
+   
+
+#scan("10.0.2.1/24")
+scan_2_send_2_boradcast_mac("10.0.2.0/24")
